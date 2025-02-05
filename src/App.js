be81fc52c17b2e3de9eaebb5dd7861d322cc4947
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-02-05 01:40:26 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+   <>
+   <h1 className='text-3xl text-blue-500 font-extrabold'>
+    Hello webpack
+   </h1>
+   </>
+  );
+}
+
+export default App;
